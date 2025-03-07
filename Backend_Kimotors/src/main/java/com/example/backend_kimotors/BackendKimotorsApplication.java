1d739d350561c1045bb164734933166181d9300d
JaVergelVe <javergelve@ufpso.edu.co>
2025-03-06 21:56:58 -0500

    Guardando cambios en BackendKimotorsApplication.java

--- Backend_Kimotors/src/main/java/com/example/backend_kimotors/BackendKimotorsApplication.java
+++ Backend_Kimotors/src/main/java/com/example/backend_kimotors/BackendKimotorsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendKimotorsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendKimotorsApplication.class, args);
     }
 
