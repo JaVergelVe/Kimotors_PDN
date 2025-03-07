f93eb306dcd8c5311afaa371ca4b5641a6419366
jjquinp <jjquinp@ufpso.edu.co>
2025-03-06 19:24:24 -0500

    Se creó el proyecto de spring

---
+++ Backend_Kimotors/src/main/java/com/example/Backend_Kimotors/BackendKimotorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Kimotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKimotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKimotorsApplication.class, args);
+	}
+
+}
